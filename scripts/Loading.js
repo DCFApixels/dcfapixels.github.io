9236ab33de2f00aee3261d119e8d409e8e12396a
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-02-01 17:00:27 +0800

    Update Loading.js

--- scripts/Loading.js
+++ scripts/Loading.js
@@ -1,4 +1,11 @@
+let _isInit = false;
+let _timeout = 6 * 1000; //6 seconds
+
 function init() {
+    if(_isInit)
+        return;
+
+    _isInit = true;
     setTimeout(function() {
         if (arguments.callee.done) 
             return;
@@ -8,23 +15,6 @@ function init() {
         elem.classList.remove("display-fixed");
     }, 1);
 };
-/*
-//для Mozilla/Firefox/Opera 9 
-if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", init, false);
-}*/
-
-/* для Internet Explorer */
-/*@cc_on@*/
-/*@if (@_win32)
-document.write("<script id=\"__ie_onload\" defer=\"defer\" src=\"java script:void(0)\"><\/script>");
-var script = document.getElementById("__ie_onload");
-script.onreadystatechange = function () {
-if (this.readyState == "complete") {
-init(); $('.main').show();
-}
-};
-/*@end@*/
 
 /* для Safari */
 if (/WebKit/i.test(navigator.userAgent)) { // условие для Safari
@@ -42,4 +32,8 @@ window.onload = init;
 let elem = document.querySelector('.load-screen');
 elem.classList.add("display-fixed");
 elem.classList.remove("hidden");
+
+setTimeout(function() {
+    init();
+}, _timeout);
 
