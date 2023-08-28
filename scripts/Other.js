9c6f0c381e441364ac3911bd3a31211ca9eb6c55
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-08-28 21:13:18 +0800

    fix style

--- scripts/Other.js
+++ scripts/Other.js
@@ -7,3 +7,15 @@ function IsMobileDevice(){
         return false;
     }
 }
+
+//var bgLayer = document.getElementById("background_layer_1");
+//var updateF = function() {
+//    const scrollY = window.scrollY;
+//    var y = (scrollY / 2) % 1200;
+//    console.log(y);
+//
+//    bgLayer.style.top = `${y}px`;
+//    //bgLayer.style.backgroundPositionY = `${y}px`;
+//    //bgLayer.style.transform = `translateY(${y}px)`;
+//}
+//window.addEventListener('scroll', updateF);
